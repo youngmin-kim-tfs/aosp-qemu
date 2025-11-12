7d37965b5e40650a82f83f1b10da49db34fd0654
youngmin-kim-tfs <60011426+youngmin-kim-tfs@users.noreply.github.com>
2025-11-13 04:47:45 +0900

    CHANGE: testing vm heap size 896MB

--- android/android-emu/android/main-common.c
+++ android/android-emu/android/main-common.c
@@ -1325,7 +1325,7 @@ static bool emulator_handleCommonEmulatorOptions(AndroidOptions* opts,
     int minVmHeapSize = minRamVmHeapSize > minApiLevelVmHeapSize
                                 ? minRamVmHeapSize
                                 : minApiLevelVmHeapSize;
-    const int maxVmHeapSize = 4 * minVmHeapSize > 576 ? 576 : 4 * minVmHeapSize;
+    const int maxVmHeapSize = 4 * minVmHeapSize > 896 ? 896 : 4 * minVmHeapSize;
 
     if (minVmHeapSize > maxVmHeapSize) {
         minVmHeapSize = maxVmHeapSize;
